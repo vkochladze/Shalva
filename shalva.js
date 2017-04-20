a2014017ba3300ac31a7f25469ff7af56aee2046
Vano Qochladze <vkochladze@gmail.com>
2017-04-20 15:40:20 +0400

    CCC

---
+++ shalva.js
@@ -0,0 +1,12 @@
+var day = new Date();
+var dayGet = day.getDate();
+var monthGet = day.getMonth(); 
+
+if (dayGet == 19 && monthGet == 3) {
+	document.getElementById("demo").innerText = "YES";
+} else {
+	document.getElementById("demo").innerText = "NO";
+}
+
+console.log("%cდიდი სიყვარულით ვანოსგან საუკეთესო მამას", "font-size:64px;");
+//console.log("%cExtra Large Yellow Text with Red Background", "background: red; color: yellow; font-size: x-large");
